af4738ddf27a2d2ef5e23097abc159e0d45ecc85
cwern40 <chris.wernli@hotmail.com>
2019-08-28 17:18:29 -0600

    I added the findSteps function

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,8 @@ const db = require('../data/db-config');
 
 module.exports = {
     find,
-    findById
+    findById,
+    findSteps
 };
 
 function find() {
@@ -13,8 +14,13 @@ function findById(id) {
     return db('schemes').where({ id }).first();
 }
 
-function findSteps() {
-
+function findSteps(scheme_id) {
+    return db('steps as s')
+        .join('schemes as c', 'c.id', 's.scheme_id')
+        .select('s.id', 'c.scheme_name', 's.step_number', 's.instructions')
+        .where({ scheme_id })
+        .orderBy('s.step_number')
+        
 }
 
 function add() {
