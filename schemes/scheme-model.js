43f6f64b3083d3d55f061490c6efdc112bbbc9e7
cwern40 <chris.wernli@hotmail.com>
2019-08-28 16:59:53 -0600

    I created the findById funtion

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,30 @@
 const db = require('../data/db-config');
 
 module.exports = {
-    find
+    find,
+    findById
 };
 
 function find() {
     return db('schemes');
+}
+
+function findById(id) {
+    return db('schemes').where({ id }).first();
+}
+
+function findSteps() {
+
+}
+
+function add() {
+
+}
+
+function update() {
+
+}
+
+function remove() {
+
 }
